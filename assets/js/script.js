79d866b32a4976130ab26bba83aab0e7bc5d051a
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2024-08-17 01:22:43 -0400

    got all elements to line up in one column when in mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const overlay = document.querySelector('.overlay')
     const closebtn = document.querySelector('.closebtn')
     const body = document.querySelector('body')
+    const pageContent = document.querySelector('.page-content');
+    const navLinks = document.querySelectorAll('a');
 
     hamburger.addEventListener('click', function() {
         overlay.style.display = 'flex';
         body.classList.add('no-scroll')
         hamburger.style.display = 'none';
-    })
+    });
 
     closebtn.addEventListener('click', function() {
         overlay.style.display = 'none';
         body.classList.remove('no-scroll');
         hamburger.style.display = 'block';
-    })
-});
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            pageContent.classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = this.href;
+            }, 500);
+        });
+    });
+
+    pageContent.classList.add('fade-in');
+})
