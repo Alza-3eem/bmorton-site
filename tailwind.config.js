68852e3da31cee973d9a1be31c90a9ec9f2d2c88
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2024-08-19 14:06:50 -0400

    added tailwind form

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',              // Paths to your HTML files
+    './assets/css/**/*.css', // Paths to your CSS files
+    './assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
