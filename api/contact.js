8176a617f9236e0c0ce729c8767edea378a524ca
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2025-07-22 18:48:36 -0400

    updated contact info

--- api/contact.js
+++ api/contact.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
         const mailOptions = {
             from: email,
-            to: 'mortonjt2@gmail.com',
+            to: 'bmortoncounseling@gmail.com',
             subject: `New message from ${name}`,
             text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
         };
